ad95ef63f5c106ef349e4d293199a50d33f25db6
Rosie <jayjaygames40@gmail.com>
2023-11-17 14:36:54 -0600

    Fixed texture filtering

--- src/engine/texture.c
+++ src/engine/texture.c
@@ -9,8 +9,8 @@ void texture_create_file(struct texture *t, const char *path)
 	glBindTexture(GL_TEXTURE_2D, t->id);
 	t->spr = sprite_load(path);
 	assertf(t->spr, "Failed to load texture from '%s'\n", path);
+	glSpriteTextureN64(GL_TEXTURE_2D, t->spr, NULL);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glSpriteTextureN64(GL_TEXTURE_2D, t->spr, NULL);
 	glBindTexture(GL_TEXTURE_2D, 0);
 }
